1c92c2aee329c254280e01f6febd6640367f5cd3
Rony Silva <rony.silva@grupoboticario.com.br>
2022-06-16 21:04:15 -0300

    feat: learning modules

---
+++ 2-modules/dist/bundle.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";const o=5+5;console.log(0),console.log(o)})();
---
+++ 2-modules/main.js
@@ -0,0 +1,7 @@
+import { add, subtract } from './functions';
+
+const output = subtract(5,5);
+const output2 = add(5,5);
+
+console.log(output);
+console.log(output2);
